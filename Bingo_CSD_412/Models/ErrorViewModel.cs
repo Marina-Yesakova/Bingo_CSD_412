de503fb5d9a7d38c7e65f59f75afb951de3c82c2
Marina-Yesakova <esakova.marina@gmail.com>
2018-10-19 22:13:35 -0700

    Initial commit
    Creating new project

---
+++ Bingo_CSD_412/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bingo_CSD_412.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
