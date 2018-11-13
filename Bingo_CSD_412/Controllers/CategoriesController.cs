7a16a37dcf22a85216e2624361b39175c6075202
Marina-Yesakova <esakova.marina@gmail.com>
2018-11-12 21:25:24 -0800

    Add categories View

---
+++ Bingo_CSD_412/Controllers/CategoriesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Bingo_CSD_412.Models;
+
+namespace Bingo_CSD_412.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            CategoryManager categoryManager = new CategoryManager();
+            Category[] categories = categoryManager.ListAllCategories();
+            return View(categories);
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(String categoryName)
+        {
+            return View(Category.CSD_412_Category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Bingo_CSD_412/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bingo_CSD_412.Models
+{
+    public class Category
+    {
+        //static category TODO: have to implement in Database 
+        public static readonly Category CSD_412_Category = new Category();
+        public String Name { get; set; }
+        public String[] Words { get; set; }
+
+        static Category()
+        {
+            CSD_412_Category.Name = "CSD_412";
+            CSD_412_Category.Words = new[] { "HTTP" , "URL", "Data Tier", "N Tier", "Application Settings" };
+        }
+    }
+}
