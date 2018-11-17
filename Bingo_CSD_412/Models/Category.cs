1b14f7c23c482d0bc841d92abb6bb82e2629ab4a
John C Lawson <44327899+JohnLawsonLwtech@users.noreply.github.com>
2018-11-16 20:59:46 -0800

    Update Category.cs

--- Bingo_CSD_412/Models/Category.cs
+++ Bingo_CSD_412/Models/Category.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,12 +10,62 @@ namespace Bingo_CSD_412.Models
         //static category TODO: have to implement in Database 
         public static readonly Category CSD_412_Category = new Category();
         public String Name { get; set; }
-        public String[] Words { get; set; }
+        public HashSet<string> Words { get; set; }
+        public int Id { get; }
 
-        static Category()
+        static Category() //Kept this for testing purposes
         {
             CSD_412_Category.Name = "CSD_412";
-            CSD_412_Category.Words = new[] { "HTTP" , "URL", "Data Tier", "N Tier", "Application Settings" };
+            CSD_412_Category.Words.Add("HTTP");
+            CSD_412_Category.Words.Add("URL");
+            CSD_412_Category.Words.Add("Date Tier");
+            CSD_412_Category.Words.Add("N Tier");
+            CSD_412_Category.Words.Add("Application Settings");
+        }
+
+        /*public Category()  <-- Real constructor
+        {
+            Id = GenerateId();
+            Name = GetName();
+            Words = new HashSet<string>();
+        }*/
+
+        private string GetName()
+        {
+            //TODO: Add funcationality that allows user to name their category
+            //Return that user input as a String
+            throw new NotImplementedException();
+        }
+
+        private int GenerateId()
+        {
+            //TODO: Retrive from database the count of categories, and increament by one
+            //return CategoryCount++;
+            throw new NotImplementedException();
+        }
+
+
+        //TODO: View must implement a GUI for the user to send information to the Model
+        public void AddWord(string word) //TODO: Implement an update to database
+        {   
+            Words.Add(word);
+        }
+
+        public void EditWord(string oldword, string newword) //TODO: Implement an update to database
+        {
+            if (Words.Contains(oldword))
+            {
+                if(!Words.Contains(newword))
+                {
+                    Words.Remove(oldword);
+                    Words.Add(newword);
+                }
+            }
+        }
+
+        public void RemoveWord(string word) //TODO: Implement an update to database
+        {
+            Words.Remove(word);
         }
     }
 }
