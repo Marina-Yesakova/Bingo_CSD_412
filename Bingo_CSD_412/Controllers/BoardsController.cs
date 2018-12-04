2dc8dfc4cc61a382b87eb5298e4e7447baba3c72
John C Lawson <44327899+JohnLawsonLwtech@users.noreply.github.com>
2018-12-03 17:13:38 -0800

    Update BoardsController.cs

--- Bingo_CSD_412/Controllers/BoardsController.cs
+++ Bingo_CSD_412/Controllers/BoardsController.cs
@@ -1,13 +1,21 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Bingo_CSD_412.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Bingo_CSD_412.Controllers
 {
-    public class BoardsController : BaseController
+    public class BoardsController : Controller
     {
         //TODO implement connect to Database here and proper dependency injection for BoardManager into BoardsController 
         public static BoardManager boardManager = new BoardManager();
+        private readonly BingoContext _db;
+
+        public BoardsController(BingoContext db)
+        {
+            _db = db;
+        }
 
         // GET: Boards
         public ActionResult Index()
@@ -17,9 +25,11 @@ namespace Bingo_CSD_412.Controllers
         }
 
         // GET: Boards/Generate/CSD_412
-        public ActionResult Generate(String category)
+        public async System.Threading.Tasks.Task<ActionResult> GenerateAsync(String category)
         {
-            Board board = boardManager.GenerateBoard(5, 5, category);
+            List<Word> WordsTable = await _db.Words.ToListAsync();
+            int WordsTableSize = WordsTable.Count;
+            Board board = boardManager.GenerateBoard(5, 5, category, WordsTableSize, WordsTable);
             // returning the details page of the new board
             return RedirectToAction(nameof(Details), new { boardId = board.BoardId });
         }
@@ -28,21 +38,14 @@ namespace Bingo_CSD_412.Controllers
         public ActionResult Details(int boardId)
         {
             Board board = boardManager.GetBoardById(boardId);
-           
-            if (ReturnJson())
-            {
-                return Json(board);
-            }
-            else
-            {
-                return View(board);
-            }
+            return View(board);
         }
 
         //logic for crossing the word in Board
         [HttpPost]
         public ActionResult CrossWord(int boardId, int wordIndex)
         {
+
             Board board = boardManager.CrossWordInBoard(boardId, wordIndex);
             // returning the details page of the new board
             return RedirectToAction(nameof(Details), new { boardId = board.BoardId });
